2e86bafd7fcc5b2a52b650a475a844b1e1f1dc33
Paola Garcia <paoga.87@gmail.com>
2017-01-04 11:10:24 -0500

    Create secure-form.php

---
+++ secure-form.php
@@ -0,0 +1,54 @@
+//PHP secure form - No validation implemented
+<!DOCTYPE HTML>  
+<html>
+    <head>
+    </head>
+  <body>  
+
+    <?php
+      // define variables and set to empty values
+      $name = $email = $gender = $comment = "";
+
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = test_input($_POST["name"]);
+        $email = test_input($_POST["email"]);
+        $comment = test_input($_POST["comment"]);
+        $gender = test_input($_POST["gender"]);
+      }
+
+      function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+    ?>
+
+    <h2>Testing Form</h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+      Name: <input type="text" name="name">
+      <br><br>
+      E-mail: <input type="text" name="email">
+      <br><br>
+      Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+      <br><br>
+      Gender:
+      <input type="radio" name="gender" value="female">Female
+      <input type="radio" name="gender" value="male">Male
+      <br><br>
+      <input type="submit" name="submit" value="Submit">  
+    </form>
+
+    <?php
+      echo "<h2>Your Input:</h2>";
+      echo $name;
+      echo "<br>";
+      echo $email;
+      echo "<br>";
+      echo $comment;
+      echo "<br>";
+      echo $gender;
+    ?>
+
+   </body>
+</html>
